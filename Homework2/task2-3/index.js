891722009c086e6ee522858bcb20795f658dab1e
tatcka <tatka_ov@ukr.net>
2015-06-09 13:01:05 +0300

    hw3

--- Homework2/task2-3/index.js
+++ Homework2/task2-3/index.js
@@ -7,10 +7,44 @@ var mainGame;
 
 var cellList; // Contains the list of cells
 
-var model
+var model =
 {
+  fieldSize: 0,
+  cellList: [],
+  gameStatus: 'notStarted',
+  lastTurn: 'o',
+  winnerMessage: ''
+};
+
+function saveGame() {
+  'use strict';
+  var data = JSON.stringify(model);
+  localStorage.setItem('game', data);
 }
-;
+
+function restoreCells() {
+  'use strict';
+  var i;
+  var cell;
+  var modelValue;
+
+  for (i = 0; i < cellList.length; i++) {
+    cell = cellList[i];
+    modelValue = getModelCell(cell.getAttribute('x'), cell.getAttribute('y')).value;
+    if (modelValue !== '') {
+      cell.classList.add(modelValue);
+    }
+  }
+}
+
+function loadGame() {
+  'use strict';
+  var data = localStorage.getItem('game');
+  if (data !== null) {
+    model = JSON.parse(data);
+    changeState();
+  }
+}
 
 // Shows winner message
 function showWinnerMessage(message) {
@@ -21,7 +55,8 @@ function showWinnerMessage(message) {
 function endGame(message) {
   'use strict';
   showWinnerMessage(message);
-  gameIsEnd = true;
+  model.gameStatus = 'gameEnded';
+  model.winnerMessage = message;
 }
 
 // Checks conditions for the game over
@@ -54,71 +89,134 @@ function isInteger(obj) {
   return !isNaN(obj) && (typeof obj === 'number') && (obj % 1 === 0);
 }
 
-function getRow(size) {
+function buildFieldDiv() {
   'use strict';
   var i;
+  var j;
   var cell;
-  var rowDiv = document.createElement('div');
-  rowDiv.classList.add('row');
 
-  for (i = 0; i < size; i++) {
-    cell = document.createElement('div');
-    cell.classList.add('cell');
-    rowDiv.appendChild(cell);
-    cellList.push(cell);
+  fieldDiv.innerHTML = '';
+  cellList = [];
+
+  for (j = 0; j < model.fieldSize; j++) {
+    var rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+
+    for (i = 0; i < model.fieldSize; i++) {
+      cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.setAttribute('x', i.toString());
+      cell.setAttribute('y', j.toString());
+      rowDiv.appendChild(cell);
+      cellList.push(cell);
+      model.cellList.push({x: i.toString(), y: j.toString(), value: ''});
+    }
+
+    fieldDiv.appendChild(rowDiv)
   }
 
   return rowDiv;
 }
 
-function onStartNewButton() {
+function changeState() {
+  'use strict';
+  switch (model.gameStatus) {
+    case 'gameStarted':
+    case 'gameEnded':
+    {
+      startMenuDiv.style.display = 'none';
+      mainGame.style.display = 'block';
+      buildFieldDiv();
+      restoreCells();
+      showWinnerMessage(model.winnerMessage);
+      break;
+    }
+    case 'notStarted':
+    {
+      fieldSizeInput.value = '';
+      model.cellList = [];
+      model.lastTurn = 'o';
+      model.cellList = [];
+      model.winnerMessage = '';
+
+      startMenuDiv.style.display = 'block';
+      mainGame.style.display = 'none';
+      break;
+    }
+  }
+  saveGame();
+}
+
+function onGenerateFieldButton() {
   'use strict';
   var fieldSize = +fieldSizeInput.value;
-  var i;
 
   if (!isInteger(fieldSize) || fieldSize < 5 || fieldSize > 15) {
     errorDiv.innerHTML = 'Вы ввели некорректное число';
     return;
   }
 
-  errorDiv.innerHTML = '';
-  startMenuDiv.style.display = 'none';
-  mainGame.style.display = 'block';
+  model.gameStatus = 'gameStarted';
+  model.fieldSize = fieldSize;
 
-  for (i = 0; i < fieldSize; i++) {
-    fieldDiv.appendChild(getRow(fieldSize));
+  buildFieldDiv(fieldSize);
+  changeState();
+}
+
+function getModelCell(x, y) {
+  'use strict';
+  var j;
+  for (j = 0; j < model.cellList.length; j++) {
+    var cell = model.cellList[j];
+    var cellX = cell.x;
+    var cellY = cell.y;
+
+    if (cellX === x && cellY === y) {
+      return cell;
+    }
   }
-
-  fieldDiv.addEventListener('click', onCellClicked);
 }
 
 function onCellClicked(e) {
   'use strict';
   var currentTurn;
+  var cell = e.target;
 
   // There is no need to do anything if the game is over or cell is not empty
-  if (gameIsEnd || e.target.classList.contains('x') || e.target.classList.contains('o') || !e.target.classList.contains('cell')) {
+  if (model.gameStatus !== 'gameStarted' || cell.classList.contains('x') || cell.classList.contains('o') || !cell.classList.contains('cell')) {
     return;
   }
 
   // define current turn according to last turn
-  if (lastTurn === 'o') {
+  if (model.lastTurn === 'o') {
     currentTurn = 'x';
   } else {
     currentTurn = 'o';
   }
 
   // Make turn and save it as last turn
-  e.target.classList.add(currentTurn);
-  lastTurn = currentTurn;
+  cell.classList.add(currentTurn);
+  model.lastTurn = currentTurn;
+
+  var cellModel = getModelCell(cell.getAttribute('x'), cell.getAttribute('y'));
+  cellModel.value = currentTurn;
 
   checkEndGame();
+  saveGame();
+}
+
+function onStartNewGameButton() {
+  'use strict';
+  model.gameStatus = 'notStarted';
+  changeState();
 }
 
 function onLoad() {
   'use strict';
-  var startNewButton = document.querySelector('.generateField');
-  startNewButton.addEventListener('click', onStartNewButton);
+  var generateFieldButton = document.querySelector('.generateField');
+  var startNewGameButton = document.querySelector('.startNewGame');
+  generateFieldButton.addEventListener('click', onGenerateFieldButton);
+  startNewGameButton.addEventListener('click', onStartNewGameButton);
 
   fieldSizeInput = document.querySelector('.count');
   errorDiv = document.querySelector('.error-message');
@@ -127,10 +225,11 @@ function onLoad() {
   fieldDiv = document.querySelector('.field');
   winnerMessageDiv = document.querySelector('.winner-message');
   mainGame = document.querySelector('.mainGame');
+  fieldDiv.addEventListener('click', onCellClicked);
 
-  gameIsEnd = false;
-  lastTurn = 'o';
   cellList = [];
+
+  loadGame();
 }
 
 window.addEventListener('load', onLoad);
